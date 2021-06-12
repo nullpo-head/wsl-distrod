084a82ab453544b8d8f44759cb1572dc14b90807
Takaya Saeki <abc.tkys+pub@gmail.com>
2021-06-13 00:19:04 +0900

    Init logger and option parser

--- cmd/src/main.rs
+++ cmd/src/main.rs
@@ -1,3 +1,72 @@
+use colored::*;
+use std::io::Write;
+use std::str::FromStr;
+
+use structopt::StructOpt;
+use strum::{EnumString, EnumVariantNames};
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "distrod")]
+pub struct Opts {
+    #[structopt(short, long)]
+    pub log_level: Option<LogLevel>,
+    #[structopt(subcommand)]
+    pub command: Subcommand,
+}
+
+#[derive(Copy, Clone, Debug, EnumString, EnumVariantNames)]
+#[strum(serialize_all = "kebab-case")]
+pub enum LogLevel {
+    Off,
+    Error,
+    Warn,
+    Info,
+    Debug,
+    Trace,
+}
+
+#[derive(Debug, StructOpt)]
+pub enum Subcommand {
+    Launch(LaunchOpts),
+}
+
+#[derive(Debug, StructOpt)]
+#[structopt(rename_all = "kebab")]
+pub struct LaunchOpts {}
+
 fn main() {
-    println!("Hello, world!");
+    let opts = Opts::from_args();
+    init_logger(&opts.log_level);
+    log::info!("Hello, world!");
+}
+
+fn init_logger(log_level: &Option<LogLevel>) {
+    let mut env_logger_builder = env_logger::Builder::new();
+
+    if let Some(ref level) = log_level {
+        env_logger_builder.filter_level(
+            log::LevelFilter::from_str(
+                <LogLevel as strum::VariantNames>::VARIANTS[*level as usize],
+            )
+            .unwrap(),
+        );
+    } else {
+        env_logger_builder.filter_level(log::LevelFilter::Info);
+    }
+
+    env_logger_builder.format(move |buf, record| {
+        writeln!(
+            buf,
+            "{}{} {}",
+            "[Distrod]".bright_green(),
+            match record.level() {
+                log::Level::Info => "".to_string(),
+                log::Level::Error | log::Level::Warn =>
+                    format!("[{}]", record.level()).red().to_string(),
+                _ => format!("[{}]", record.level()).bright_green().to_string(),
+            },
+            record.args()
+        )
+    });
+    env_logger_builder.init();
 }
